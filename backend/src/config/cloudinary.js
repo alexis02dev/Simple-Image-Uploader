98ccea31db4dc0afee6fe9121189840b18661d8f
alexis02dev <alexis02dev@gmail.com>
2025-10-26 01:16:49 -0400

    refactor(backend): add comments to cloudinary.js for improved clarity on configuration settings

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,9 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
-
+/**
+ * Configurar Cloudinary
+ */
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Nombre del cloud
+  api_key: process.env.CLOUDINARY_API_KEY, // API key
+  api_secret: process.env.CLOUDINARY_API_SECRET, // API secret
 });
 
-export default cloudinary;
+export default cloudinary; // Exportar cloudinary

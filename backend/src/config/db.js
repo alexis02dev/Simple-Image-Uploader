09705fb44f2c9ac3ff50032ee814688987acea7d
alexis02dev <alexis02dev@gmail.com>
2025-10-26 01:04:57 -0400

    refactor(backend): add comments to db.js for improved clarity on MongoDB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-
+/**
+ * Conectar a la base de datos MongoDB Atlas
+ */
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGOOSE_URL);
-    console.log("✅ Conectado a MongoDB Atlas");
+    await mongoose.connect(process.env.MONGOOSE_URL); // Conectar a la base de datos MongoDB Atlas -> mongoose.connect(process.env.MONGOOSE_URL)
+    console.log("✅ Conectado a MongoDB Atlas"); // Si se conecta correctamente, mostrar un mensaje de éxito
   } catch (error) {
-    console.error("❌ Error conectando a MongoDB:", error.message);
-    process.exit(1);
+    console.error("❌ Error conectando a MongoDB:", error.message); // Si hay un error, mostrar un mensaje de error -> console.error("❌ Error conectando a MongoDB:", error.message)
+    process.exit(1); // Si hay un error, salir del proceso con un código de error 1 -> process.exit(1) -> exit(1)
   }
 };

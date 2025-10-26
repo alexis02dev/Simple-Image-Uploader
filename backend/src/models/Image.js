3f8f055ebbb07eb7555cb2295422dc4f0115a9f8
alexis02dev <alexis02dev@gmail.com>
2025-10-26 01:02:50 -0400

    refactor(backend): add comments and indexing to image schema for clarity and performance

--- backend/src/models/Image.js
+++ backend/src/models/Image.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
-
+/**
+ * Schema para la colección de imágenes en la base de datos MongoDB
+ * Recibe: filename, url, public_id, uploadedAt
+ */
 const imageSchema = new mongoose.Schema(
   {
-    filename: { type: String, required: true },
-    url: { type: String, required: true },
-    public_id: { type: String, required: true },
-    uploadedAt: { type: Date, default: Date.now },
+    filename: { type: String, required: true }, // Nombre del archivo
+    url: { type: String, required: true }, // URL de la imagen
+    public_id: { type: String, required: true }, // ID de la imagen en Cloudinary
+    uploadedAt: { type: Date, default: Date.now }, // Fecha de subida
   },
-  { versionKey: false }
+  { versionKey: false } // No mostrar el campo versionKey
 );
+// Crear un índice en el campo filename para que la búsqueda sea más rápida
+imageSchema.index({ filename: 1 }); // 1: Ascendente, -1: Descendente
 
-export default mongoose.model("Image", imageSchema);
+export default mongoose.model("Image", imageSchema); // Exportar el modelo de la colección de imágenes como Image con el schema imageSchema -> mongoose.model("Image", imageSchema) -> mongoose.model("Image", imageSchema, "images")

6767732bb725fdc311920c4b34df49aa54cafc6d
alexis02dev <alexis02dev@gmail.com>
2025-10-26 02:19:36 -0400

    docs(backend): add comment to cloudinary.js for dotenv usage clarification

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,6 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 
+// Uso de dotenv para obtener las variables de entorno
 dotenv.config();
 /**
  * Configurar Cloudinary

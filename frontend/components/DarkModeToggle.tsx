826f95979c75f2640d166d89abddb9161a9d9997
alexis02dev <alexis02dev@gmail.com>
2025-10-28 16:37:10 -0400

    feat(frontend): add component DarkModeToggle

---
+++ frontend/components/DarkModeToggle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DarkModeToogle = () => {
+  return (
+    <div>DarkModeToogle</div>
+  )
+}
+
+export default DarkModeToogle
---
+++ frontend/components/ImageUploader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageUploader = () => {
+  return (
+    <div className='mt-8'>ImageUploader</div>
+  )
+}
+
+export default ImageUploader
